2a2acb85d1b637e417158ab74cd3ba9855eae637
Vinicius Emanoel Andrade <viniciusemanoelandrade@hotmail.com>
2020-04-04 00:01:01 -0300

    Fragmentando a view em funções; Adicionando a classe tabela

--- src/app/App.kt
+++ src/app/App.kt
@@ -3,106 +3,100 @@ package app
 import kotlinx.html.id
 import react.*
 import react.dom.*
-import logo.*
-import ticker.*
 
-class App : RComponent<RProps, RState>() {
+interface AppState : RState {
+
+}
+
+class App : RComponent<RProps, AppState>() {
     override fun RBuilder.render() {
+        appHeaderUi()
+        containerUi()
+    }
+
+    private fun RBuilder.appHeaderUi() {
         div("App-header") {
             h2 {
                 +"Tabela de Simulação"
             }
         }
+    }
 
+    private fun RBuilder.containerUi() {
         div("container") {
-            div("row borderRow") {
-                div("col-3") {}
-                div("col-6") {
-                    form {
-                        div("form-group") {
+            formUi()
+            tableUi()
+        }
+    }
+
+    private fun RBuilder.formUi() {
+        div("row borderRow") {
+            div("col-3") {}
+            div("col-6") {
+                form {
+                    div("form-group") {
+                        input(classes = "form-control") {
+                            attrs.placeholder = "Tempo de Simulação (minutos)"
+                        }
+                    }
+                    div("form-row") {
+                        div("form-group col-md-6") {
                             input(classes = "form-control") {
-                                attrs.placeholder = "Tempo de Simulação (minutos)"
+                                attrs.placeholder = "TEC"
+                            }
+                            button(classes = "btn btn-secondary btn-sm") {
+                                +"Adicionar"
                             }
                         }
-                        div("form-row") {
-                            div("form-group col-md-6") {
-                                input(classes = "form-control") {
-                                    attrs.placeholder = "TEC"
-                                }
-                                button(classes = "btn btn-secondary btn-sm") {
-                                    +"Adicionar"
-                                }
-                            }
-                            div("form-group col-md-6") {
-                                textArea(classes = "form-control") {
-                                }
+                        div("form-group col-md-6") {
+                            textArea(classes = "form-control") {
                             }
                         }
-                        div("form-row") {
-                            div("form-group col-md-6") {
-                                input(classes = "form-control") {
-                                    attrs.placeholder = "TS"
-                                }
-                                button(classes = "btn btn-secondary btn-sm") {
-                                    +"Adicionar"
-                                }
+                    }
+                    div("form-row") {
+                        div("form-group col-md-6") {
+                            input(classes = "form-control") {
+                                attrs.placeholder = "TS"
                             }
-                            div("form-group col-md-6") {
-                                textArea(classes = "form-control") {
+                            button(classes = "btn btn-secondary btn-sm") {
+                                +"Adicionar"
+                            }
+                        }
+                        div("form-group col-md-6") {
+                            textArea(classes = "form-control") {
 
-                                }
                             }
                         }
+                    }
 
-                        button(classes = "btn btn-secondary btn-lg btn-block") {
-                            attrs.id = "buttonSimular"
-                            +"Simular"
-                        }
+                    button(classes = "btn btn-secondary btn-lg btn-block") {
+                        attrs.id = "buttonSimular"
+                        +"Simular"
                     }
                 }
-                div("col-3") {}
             }
+            div("col-3") {}
+        }
+    }
 
-            div("row") {
-                table(classes = "table table-striped table-dark table-bordered") {
-                    thead {
-                        tr {
-                            th(classes = "border align-middle") { +"Cliente" }
-                            th(classes = "border align-middle") { +"Tempo desde a última chegada" }
-                            th(classes = "border align-middle") { +"Tempo de chegada no Relógio da Simulação" }
-                            th(classes = "border align-middle") { +"Tempo de Serviço" }
-                            th(classes = "border align-middle") { +"Tempo do ínicio do serviço no relógio da simulação" }
-                            th(classes = "border align-middle") { +"Tempo do cliente na fila" }
-                            th(classes = "border align-middle") { +"Tempo final do serviço no relógio da simulação" }
-                            th(classes = "border align-middle") { +"Tempo do cliente no sistema (minutos)" }
-                            th(classes = "border align-middle") { +"Tempo livre do operador (minutos)" }
-                        }
+    private fun RBuilder.tableUi() {
+        div("row") {
+            table(classes = "table table-striped table-dark table-bordered") {
+                thead {
+                    tr {
+                        th(classes = "border align-middle") { +"Cliente" }
+                        th(classes = "border align-middle") { +"Tempo desde a última chegada" }
+                        th(classes = "border align-middle") { +"Tempo de chegada no Relógio da Simulação" }
+                        th(classes = "border align-middle") { +"Tempo de Serviço" }
+                        th(classes = "border align-middle") { +"Tempo do ínicio do serviço no relógio da simulação" }
+                        th(classes = "border align-middle") { +"Tempo do cliente na fila" }
+                        th(classes = "border align-middle") { +"Tempo final do serviço no relógio da simulação" }
+                        th(classes = "border align-middle") { +"Tempo do cliente no sistema (minutos)" }
+                        th(classes = "border align-middle") { +"Tempo livre do operador (minutos)" }
                     }
-                    tbody {
-                        tr {
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                        }
+                }
+                tbody {
 
-                        tr {
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                            td { +"Teste" }
-                        }
-                    }
                 }
             }
         }
